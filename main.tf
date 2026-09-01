c37aa4fab188521f5ccc0f6937175532d7273a31
Moulali-bunny <moulali2911@gmail.com>
2026-09-01 15:23:51 +0530

    Comment out aws_instance resource definition

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "three" {
   }
 }
 
-*/resource "aws_instance" "four" {
+/*resource "aws_instance" "four" {
   ami             = "ami-081b0a6eac00b4f53"
   instance_type   = "t3.micro"
   key_name        = "ubuntu"
